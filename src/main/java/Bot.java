da58ea12a1f2535d6b920fe7a55a81d3407df243
master-kot <master-kot@mail.ru>
2020-05-12 00:08:18 +0300

    Bot was changed for using the proxy server

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,27 +1,55 @@
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.ApiContext;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 /**
  * Token получен у бота BotFather командой /newbot
  * Адрес бота в телеграм: t.me/ScheduleHelpMeBot
  * Api бота: github.com/rubenlagus/TelegramBots/
  */
-public class Bot  extends TelegramLongPollingBot {
+public class Bot extends TelegramLongPollingBot {
 
     public static void main(String[] args) {
-        ApiContextInitializer.init(); //инициализируем Api Context
-        TelegramBotsApi botsApi = new TelegramBotsApi(); //получаем Telegram Bots API
+        //инициализируем Api Context
+        ApiContextInitializer.init();
+        //получаем Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
         try {
+            //Регистрируем бота
             botsApi.registerBot(new Bot());
-        } catch (TelegramApiRequestException e) {
-            e.printStackTrace(); //регистрируем бота
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static final String BOT_NAME = "ScheduleHelpMeBot";
+    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
+
+    private static final String PROXY_HOST = "catalog.live.ovh" /* proxy host */;
+    private static final Integer PROXY_PORT = 443 /* proxy port */;
+
+    protected Bot(DefaultBotOptions botOptions) {
+        super(botOptions);
+    }
+
+    protected Bot() {
+        super();
+    }
+
+    private static DefaultBotOptions setProxyParameters() {
+            //Устанавливаем параметры Http proxy
+            DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
+            botOptions.setProxyHost(PROXY_HOST);
+            botOptions.setProxyPort(PROXY_PORT);
+            //Выбрать тип прокси: [HTTP|SOCKS4|SOCKS5]
+            botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+            return botOptions;
     }
 
     /**
@@ -68,11 +96,11 @@ public class Bot  extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "ScheduleHelpMeBot";
+        return BOT_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return "token";
+        return BOT_TOKEN;
     }
 }
